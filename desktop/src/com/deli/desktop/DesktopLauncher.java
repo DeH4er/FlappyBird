d83e038961bd0f12c95d09e845fc922c2466135d
Denys Lytviak <deh4er@gmail.com>
2017-04-28 19:51:48 +0200

    First commit

---
+++ desktop/src/com/deli/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.deli.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.deli.FlappyBirdGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = FlappyBirdGame.WIDTH;
+		config.height = FlappyBirdGame.HEIGHT;
+		config.title = FlappyBirdGame.TITLE;
+		new LwjglApplication(new FlappyBirdGame(), config);
+	}
+}
